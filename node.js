b589a55e96f58ae6f4815ea958b9f75497a1e823
Oreology <hafidhsatyanto@gmail.com>
2021-04-03 20:15:47 +0700

    oops port

--- node.js
+++ node.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.port || 3000
+const PORT = process.env.PORT || 5000
 
 const { Pool } = require('pg');
 const pool = new Pool({
