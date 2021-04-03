9df7f6a4ed9446aaf2d5f3356bfff133ffb7ddf2
Oreology <hafidhsatyanto@gmail.com>
2021-04-03 20:03:59 +0700

    heroku db test

--- node.js
+++ node.js
@@ -1,30 +1,63 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const dbqueries = require('./queries')
+const path = require('path')
+const PORT = process.env.port || 3000
 
-const app = express()
-const port = process.env.PORT || 3000
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
-app.use(express.json())
-app.use(express.urlencoded({
-    extended: true
-}));
-
-app.get('/', (request, response) => {
-    response.json({ info: "Hello! Currently using Node.js, Express, and Postgres API" })
-})
-
-
-
-// API queries
-// app.get('/users', db.getUsers)
-// app.get('/users/:id', db.getUserById)
-// app.post('/users', db.createUser)
-// app.put('/users/:id', db.updateUser)
-// app.delete('/users/:id', db.deleteUser)
-
+express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/db', async (req, res) => {
+        try {
+            const client = await pool.connect();
+            const result = await client.query('SELECT * FROM test_table');
+            res.render('pages/db', results);
+            client.release();
+        } catch (err) {
+            console.error(err);
+            res.send("Error " + err);
+        }
+    })
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
-app.listen(port, () => {
-    console.log('App running on port ${port}.')
-})
+
+
+
+// const express = require('express')
+// const bodyParser = require('body-parser')
+// const dbqueries = require('./queries')
+
+// const app = express()
+// const port = process.env.PORT || 3000
+
+// app.use(express.json())
+// app.use(express.urlencoded({
+//     extended: true
+// }));
+
+// app.get('/', (request, response) => {
+//     response.json({ info: "Hello! Currently using Node.js, Express, and Postgres API" })
+// })
+
+
+
+// // API queries
+// // app.get('/users', db.getUsers)
+// // app.get('/users/:id', db.getUserById)
+// // app.post('/users', db.createUser)
+// // app.put('/users/:id', db.updateUser)
+// // app.delete('/users/:id', db.deleteUser)
+
+
+
+// app.listen(port, () => {
+//     console.log('App running on port ${port}.')
+// })
