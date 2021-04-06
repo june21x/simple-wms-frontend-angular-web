c4c47351ab70af8bb92dbcd5ad0d194eb9d7e4dd
Oreology <hafidhsatyanto@gmail.com>
2021-04-06 12:55:16 +0700

    temp fix

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ var db = require('../queries');
 router.get('/api/shipments', db.getIncomingShipments);
 router.get('/api/shipments/:id', db.getSingleShipment);
 router.post('/api/shipments', db.createShipment);
-router.put('/api/shipments/:id', db.updateShipment);
-router.delete('/api/shipments/:id', db.removeShipment);
+//router.put('/api/shipments/:id', db.updateShipment);
+//router.delete('/api/shipments/:id', db.removeShipment);
 
 router.get('/', function (req, res) {
     res.render('home')
