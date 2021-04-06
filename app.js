1606fb4ba520c1910f89b96cb2d37d6b70da159c
Oreology <hafidhsatyanto@gmail.com>
2021-04-06 11:10:03 +0700

    express bodyparser fix

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.json())
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/', routes);
 
