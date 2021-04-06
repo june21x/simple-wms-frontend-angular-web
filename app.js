e5fb6cd4197d68380f1cc62b636a43dd8de02c48
Oreology <hafidhsatyanto@gmail.com>
2021-04-06 11:32:30 +0700

    router render fix attempt #5

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ var routes = require('./routes/index');
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,16 @@
-const express = require('express');
-const path = require('path')
-//const app = require('../app');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
 var db = require('../queries');
 
-
-router.get('/api/shipments', db.getIncomingShipments);
-router.get('/api/shipments/:id', db.getSingleShipment);
-router.post('/api/shipments', db.createShipment);
+// router.get('/api/shipments', db.getIncomingShipments);
+// router.get('/api/shipments/:id', db.getSingleShipment);
+// router.post('/api/shipments', db.createShipment);
 //router.put('/api/shipments/:id', db.updateShipment);
 //router.delete('/api/shipments/:id', db.removeShipment);
 
 router.get('/', function (req, res) {
-    router.use(express.static(path.join(__dirname, 'public')));
-    router.set('views', path.join(__dirname, 'views'));
-    router.set('view engine', 'ejs');
-    res.send("Testing!!!");
-})
+
+    res.render('home');
+});
 
 module.exports = router;
