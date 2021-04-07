8b460708fd972e31f3f07da64c9ac1fbff1001ec
Oreology <hafidhsatyanto@gmail.com>
2021-04-07 22:58:10 +0700

    refactor attempt 5

--- server/api/Orders.js
+++ server/api/Orders.js
@@ -1,11 +1,9 @@
 //const { Model, Knex } = require('../database');
 var database = require('../database');
-//const OrderModel = require('../models/Order');
-import { Order } from ('../models/Order');
 
 async function getAllOrders(req, res, next) {
     try {
-        const orders = await Order.query();
+        const orders = await database.ObjectionModel.Order.query();
         res.status(200).json({
             status: 'success',
             data: orders,
