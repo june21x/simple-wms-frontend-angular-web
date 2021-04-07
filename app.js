11ee77fe4b39582096f89bfbad9d1cb0c405c6aa
Oreology <hafidhsatyanto@gmail.com>
2021-04-07 14:26:33 +0700

    heroku deploy fix attempt 4

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use("/api", routes);
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
